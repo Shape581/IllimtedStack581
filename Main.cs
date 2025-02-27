044a4c2c47ae1a9af05d2ac61df05fa9115e878f
Shape581 <severusrogue5008@gmail.com>
2025-02-27 11:10:18 +0100

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Life;
+
+namespace IllimitedStack581
+{
+    public class Main : Plugin
+    {
+        public Main(IGameAPI api) : base(api) { }
+
+        public override void OnPluginInit()
+        {
+            base.OnPluginInit();
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("[IllmitedStack581] Succes - Initialisé !");
+            Console.ResetColor();
+            foreach (var elements in Nova.man.item.items)
+            {
+                elements.maxSlotCount = 999999999;
+                elements.isStackable = true;
+            }
+        }
+    }
+}
